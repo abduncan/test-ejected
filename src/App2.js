f3f9ecac1c7f29f89782298d7ce9bf438c3c2fab
Andrew Duncan <abduncan0@gmail.com>
2016-11-18 17:46:37 -0500

    done

---
+++ src/App2.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+require('./app2.css')
+
+class App2 extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="red">App2</h1>
+            </div>
+        )
+    }
+}
+
+export default App2
